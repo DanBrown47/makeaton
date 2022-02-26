7810d968bc7710ec98cd105d3c0ae21a48aab133
rijfas <rijfas01@gmail.com>
2022-02-26 13:56:08 +0530

    Hotfix : requirements update, settings for crispy forms

--- greenify/settings.py
+++ greenify/settings.py
@@ -132,6 +132,10 @@ STATICFILES_DIRS = [
     BASE_DIR / "static"
 ]
 
+
+# crispy form style
+CRISPY_TEMPLATE_PACK = 'tailwind'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
