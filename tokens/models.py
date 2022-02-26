3334f05dbe5988d5b7b1c15a13f8924a112021b1
amjadcp <amjadcp03@gmail.com>
2022-02-26 13:30:35 +0530

    Feat : app tokens created

--- tokens/models.py
+++ tokens/models.py
@@ -1,15 +1,16 @@
-from turtle import mode
 from django.db import models
 from authentication.models import User
+from .utils import upload_image
 
 # Create your models here.
 
 class Token(models.Model):
     raised_by = models.ForeignKey(User, on_delete=models.CASCADE) #any user
-    assigned_by = models.ForeignKey(User, on_delete=models.CASCADE) # admin user
-    assigned_to = models.ForeignKey(User, on_delete=models.CASCADE) # volunteer user
-    # geo_location = 
-    image = models.ImageField()
+    # assigned_by = models.ForeignKey(User, on_delete=models.CASCADE) # admin user
+    # assigned_to = models.ForeignKey(User, on_delete=models.CASCADE) # volunteer user
+    geo_latitude = models.DecimalField(max_digits=11, decimal_places=7,null=True,blank=True)
+    geo_longitude = models.DecimalField(max_digits=11, decimal_places=7,null=True,blank=True)
+    images = models.ImageField(upload_to=upload_image)
     message = models.TextField(max_length=200)
     is_done = models.BooleanField(default=False)
     raised_date = models.DateField()
