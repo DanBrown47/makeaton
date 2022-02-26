2263d25f32d55567eb538c4e04b46bbf911d1cec
rijfas <rijfas01@gmail.com>
2022-02-26 11:56:34 +0530

    Chore : authentication base setup

---
+++ authentication/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+
+class User(AbstractUser):
+    is_admin = models.BooleanField(default=False)
+    is_voluntere = models.BooleanField(default=False)
+    is_user = models.BooleanField(default=True)
