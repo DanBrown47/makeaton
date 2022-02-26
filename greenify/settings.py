2c4647952bac62daaa8e2c90ad01ee92224aa6fc
DanBrown47 <danwand47@gmail.com>
2022-02-26 10:33:23 +0530

    Secret key generation fucntion add

--- greenify/settings.py
+++ greenify/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+from django.core.management.utils import get_random_secret_key
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,12 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-&p7m4)_@dj1y2f!%g1=!xq(@&tv2v^dsoht1tg-16=*z4d_@84'
+SECRET_KEY = get_random_secret_key()
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['0.0.0.0','59.96.56.234']
 
 
 # Application definition
