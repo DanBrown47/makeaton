841c58671005a9561cfb55929057cc7d5288474f
rijfas <rijfas01@gmail.com>
2022-02-27 09:04:11 +0530

    Hotfix : static fix 3

--- greenify/urls.py
+++ greenify/urls.py
@@ -1,8 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
-from django.shortcuts import s
 from django.views.generic import TemplateView
-import settings
+from . import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
@@ -21,5 +20,3 @@ handler404 = 'authentication.views.error'
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL,
                           document_root=settings.STATIC_ROOT)
-    urlpatterns += static(settings.MEDIA_URL,
-                          document_root=settings.MEDIA_ROOT)
