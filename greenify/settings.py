9231548e4b8de4b579571dada8553d3a710dbd68
DanBrown47 <danwand47@gmail.com>
2022-02-27 08:49:07 +0530

    All host allowed

--- greenify/settings.py
+++ greenify/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = get_random_secret_key()
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['0.0.0.0', '59.96.56.234', '127.0.0.1']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
